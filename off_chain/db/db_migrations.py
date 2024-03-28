efada3c6d4e780dc76d6761d0a523e618e44b5b8
AlessioGiacconi <84578962+AlessioGiacconi@users.noreply.github.com>
2024-03-28 16:04:35 +0100

    db

--- off_chain/db/db_migrations.py
+++ off_chain/db/db_migrations.py
@@ -2,15 +2,16 @@ import sqlite3
 
 con = sqlite3.connect("ADIChain")
 cur = con.cursor()
+cur.execute("DROP TABLE IF EXISTS Users")
 cur.execute("""CREATE TABLE Users(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL,
-            )""")
-cur.execute("""
+            password TEXT NOT NULL
+            );""")
+'''cur.execute("""
             INSERT INTO Users VALUES
-            ('carlo', '123456')
-            ('marco', 'qwerty')
-            """)
+            ('1','carlo', '123456'),
+            ('2', 'marco', 'qwerty')
+            """)'''
 con.commit()
 con.close()
