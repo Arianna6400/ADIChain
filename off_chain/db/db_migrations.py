7df0da46aad0d7c4f2d605feeb2b4b159a1d7dbf
AlessioGiacconi <84578962+AlessioGiacconi@users.noreply.github.com>
2024-03-29 16:55:11 +0100

    struttura db fatta

--- off_chain/db/db_migrations.py
+++ off_chain/db/db_migrations.py
@@ -2,16 +2,84 @@ import sqlite3
 
 con = sqlite3.connect("ADIChain")
 cur = con.cursor()
-cur.execute("DROP TABLE IF EXISTS Users")
-cur.execute("""CREATE TABLE Users(
+cur.execute("DROP TABLE IF EXISTS Credentials")
+cur.execute("DROP TABLE IF EXISTS Medics")
+cur.execute("DROP TABLE IF EXISTS Patients")
+cur.execute("DROP TABLE IF EXISTS Caregivers")
+cur.execute("DROP TABLE IF EXISTS Reports")
+cur.execute("DROP TABLE IF EXISTS TreatmentPlans")
+cur.execute("DROP TABLE IF EXISTS AccessLog")
+cur.execute('''CREATE TABLE Credentials(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL
-            );""")
-'''cur.execute("""
-            INSERT INTO Users VALUES
-            ('1','carlo', '123456'),
-            ('2', 'marco', 'qwerty')
-            """)'''
+            hash_password TEXT NOT NULL,
+            role TEXT CHECK(role IN ('MEDIC', 'PATIENT', 'CAREGIVER')) NOT NULL,
+            public_key TEXT NOT NULL,
+            private_key TEXT NOT NULL
+            );''')
+cur.execute('''CREATE TABLE Medics(
+            id_medic INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            birthday TEXT NOT NULL,
+            specialization TEXT NOT NULL,
+            mail TEXT,
+            phone TEXT,
+            FOREIGN KEY(id_medic) REFERENCES Credentials(id)
+            );''')
+cur.execute('''CREATE TABLE Patients(
+            id_patient INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            birthday TEXT NOT NULL,
+            birth_place TEXT NOT NULL,
+            residence TEXT NOT NULL,
+            autonomous INTEGER CHECK(autonomous IN (0,1)) NOT NULL,
+            phone TEXT, 
+            FOREIGN KEY(id_patient) REFERENCES Credentials(id)
+            );''')
+cur.execute('''CREATE TABLE Caregivers(
+            id_caregiver INTEGER NOT NULL,
+            id_patient INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            patient_relationship TEXT NOT NULL,
+            phone TEXT,
+            FOREIGN KEY(id_caregiver) REFERENCES Credentials(id),
+            FOREIGN KEY(id_patient) REFERENCES Patients(id_patient)
+            );''')
+cur.execute('''CREATE TABLE Reports(
+            id_report INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_patient INTEGER NOT NULL,
+            id_medic INTEGER NOT NULL,
+            analyses TEXT NOT NULL,
+            diagnosis TEXT NOT NULL,
+            phone TEXT,
+            FOREIGN KEY(id_patient) REFERENCES Patients(id_patient),
+            FOREIGN KEY(id_medic) REFERENCES Medics(id_medic)
+            );''')
+cur.execute('''CREATE TABLE TreatmentPlans(
+            id_treament_plan INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_patient INTEGER NOT NULL,
+            id_medic INTEGER NOT NULL,
+            id_caregiver INTGER,
+            description TEXT NOT NULL,
+            start_date TEXT NOT NULL,
+            end_date TEXT NOT NULL,
+            FOREIGN KEY(id_patient) REFERENCES Patients(id_patient),
+            FOREIGN KEY(id_medic) REFERENCES Medics(id_medic),
+            FOREIGN KEY(id_caregiver) REFERENCES Caregivers(id_caregiver)
+            );''')
+cur.execute('''CREATE TABLE AccessLog(
+            id_access INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_utente INTEGER NOT NULL,
+            action TEXT NOT NULL,
+            timestamp TEXT NOT NULL,
+            FOREIGN KEY(id_utente) REFERENCES Credentials(id)
+            );''')
+cur.execute('''
+            INSERT INTO Credentials VALUES
+            ('1','carlo', '123456', 'MEDIC', 'aaaaa', 'bbbbbbb')
+            ''')
 con.commit()
 con.close()
