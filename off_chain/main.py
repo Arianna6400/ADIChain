93774b880724d288c1abc5f149ba2760e040f661
Arianna6400 <s1115431@studenti.univpm.it>
2024-04-15 17:41:42 +0200

    connessione ganache riuscita

--- off_chain/main.py
+++ off_chain/main.py
@@ -1,6 +1,9 @@
 from cli.cli import CommandLineInterface
+from controllers.on_chain_controller import OnChainController
 
 if __name__ == "__main__":
+    controller = OnChainController()  
+    controller.compile_and_deploy('on_chain/on_chain.sol')
     cli = CommandLineInterface()
     while True:
         cli.print_menu()
