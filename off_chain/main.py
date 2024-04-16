42dc5e6716be7d6d42a0527856d82b7788421dd6
AlessioGiacconi <84578962+AlessioGiacconi@users.noreply.github.com>
2024-04-16 16:53:37 +0200

    fixxino

--- off_chain/main.py
+++ off_chain/main.py
@@ -5,7 +5,7 @@ from session.session import Session
 if __name__ == "__main__":
     new_session = Session()
     controller = OnChainController()  
-    controller.compile_and_deploy('on_chain/on_chain.sol')
+    controller.compile_and_deploy('../on_chain/on_chain.sol')
     cli = CommandLineInterface(new_session)
     while True:
         cli.print_menu()
