fb50dc3aab4519fcfc3cd63f8d0b6ff94b609e3f
Arianna6400 <s1115431@studenti.univpm.it>
2024-04-19 17:23:12 +0200

    aggiunta in transact i metodi di gestione del contratto

--- off_chain/controllers/transact_controller.py
+++ off_chain/controllers/transact_controller.py
@@ -16,33 +16,64 @@ class TransactionController:
         controller = DeployController(self.http_provider)
         contract_source_path = os.path.join(os.path.dirname(__file__), contract_source_path)
         controller.compile_and_deploy(contract_source_path)
-        self.contract = controller.contract  # Retrieve the deployed contract to use in functions
+        self.contract = controller.contract
 
     def read_data(self, function_name, *args):
-        # Calls a read-only function on the contract. These functions do not modify blockchain state and require no gas.
         function = self.contract.functions[function_name](*args)
         return function.call()
 
     def write_data(self, function_name, from_address, *args, gas=2000000, gas_price=None, nonce=None):
-        # Prepares the transaction parameters including from address, gas limit, gas price, and nonce.
         tx_parameters = {
             'from': from_address,
             'gas': gas,
             'gasPrice': gas_price or self.w3.eth.gas_price,
             'nonce': nonce or self.w3.eth.getTransactionCount(from_address)
         }
-        # Calls a function that modifies the blockchain state, therefore a transaction is sent.
         function = self.contract.functions[function_name](*args)
         tx_hash = function.transact(tx_parameters)
-        # Waits for the transaction to be mined and returns the receipt.
         receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
         return receipt
 
     def listen_to_event(self, event_name, callback):
-        # Sets up a filter to listen for a specified event emitted by the contract.
         event_filter = self.contract.events[event_name].createFilter(fromBlock='latest')
-        # Continuously polls for the event and calls the provided callback function when the event is detected.
         while True:
             for event in event_filter.get_new_entries():
                 callback(event)
-            time.sleep(10)  # Pause the loop to prevent spamming the network with requests.
+            time.sleep(10)
+
+    # HealthCareRecords Contract Interaction Methods
+    def pause(self, tx_args):
+        tx_hash = self.contract.functions.pause().transact(tx_args)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        return receipt
+
+    def unpause(self, tx_args):
+        tx_hash = self.contract.functions.unpause().transact(tx_args)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        return receipt
+
+    def register_medic(self, _id, _name, _lastname, _specialization, tx_args):
+        tx_hash = self.contract.functions.registerMedic(_id, _name, _lastname, _specialization).transact(tx_args)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        return receipt
+
+    def register_caregiver(self, _id, _name, _lastname, tx_args):
+        tx_hash = self.contract.functions.registerCaregiver(_id, _name, _lastname).transact(tx_args)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        return receipt
+
+    def update_patient(self, _id, _condition, tx_args):
+        tx_hash = self.contract.functions.updatePatient(_id, _condition).transact(tx_args)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        return receipt
+
+    def file_report(self, _reportId, _patientId, _medicId, _details, tx_args):
+        tx_hash = self.contract.functions.fileReport(_reportId, _patientId, _medicId, _details).transact(tx_args)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        return receipt
+
+    def create_treatment_plan(self, _planId, _patientId, _medicId, _treatmentDetails, _medication, _startDate, _endDate, tx_args):
+        tx_hash = self.contract.functions.createTreatmentPlan(_planId, _patientId, _medicId, _treatmentDetails, _medication, _startDate, _endDate).transact(tx_args)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        return receipt
+
