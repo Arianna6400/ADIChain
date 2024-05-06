ff7624b3d6a81e48ac942cb04dd465895f4c54ac
Arianna <s1115431@studenti.univpm.it>
2024-05-06 13:01:12 +0200

    alcune modifiche contratto

--- on_chain/HealthCareRecords.sol
+++ on_chain/HealthCareRecords.sol
@@ -7,7 +7,6 @@ contract HealthCareRecords {
         string lastname;
         string specialization;
         bool isRegistered;
-        uint256 offChainId;
     }
 
     struct Patient {
@@ -15,14 +14,12 @@ contract HealthCareRecords {
         string lastname;
         uint8 autonomous;
         bool isRegistered;
-        uint256 offChainId;
     }
 
     struct Caregiver {
         string name;
         string lastname;
         bool isRegistered;
-        uint256 offChainId;
     }
 
     struct Report {
@@ -50,14 +47,17 @@ contract HealthCareRecords {
     }
 
     uint256 private actionCounter = 0;
-    mapping(uint256 => Medic) public medics;
-    mapping(uint256 => Patient) public patients;
-    mapping(uint256 => Caregiver) public caregivers;
+    mapping(address  => Medic) public medics;
+    mapping(address  => Patient) public patients;
+    mapping(address  => Caregiver) public caregivers;
     mapping(uint256 => Report) public reports;
     mapping(uint256 => TreatmentPlan) public treatmentPlans;
     mapping(uint256 => ActionLog) public actionLogs;
     mapping(address => bool) public authorizedEditors;
     address public owner;
+
+    event EntityRegistered(string entityType, address indexed entityAddress);
+    event EntityUpdated(string entityType, address indexed entityAddress);
 
     event ActionLogged(uint256 indexed actionId, string actionType, address indexed initiator, uint256 indexed timestamp, string details);
 
@@ -71,16 +71,8 @@ contract HealthCareRecords {
         _;
     }
 
-    function generateOffChainId(string memory name, string memory lastname, string memory additionalData) private pure returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(name, lastname, additionalData)));
-    }
-
     function authorizeEditor(address _editor) public onlyOwner {
         authorizedEditors[_editor] = true;
-    }
-
-    function revokeEditor(address _editor) public onlyOwner {
-        authorizedEditors[_editor] = false;
     }
 
     function logAction(string memory _actionType, address _initiator, string memory _details) internal {
@@ -90,70 +82,63 @@ contract HealthCareRecords {
     }
 
     function addMedic(string memory name, string memory lastname, string memory specialization) public onlyOwner {
-        uint256 offChainId = generateOffChainId(name, lastname, specialization);
-        require(medics[offChainId].isRegistered == false, "Medic already registered");
-        medics[offChainId] = Medic(name, lastname, specialization, true, offChainId);
+        require(!medics[msg.sender].isRegistered, "Medic already registered");
+        medics[msg.sender] = Medic(name, lastname, specialization, true);
         logAction("Create", msg.sender, "Medic added");
+        emit EntityRegistered("Medic", msg.sender);
     }
 
-    function updateMedic(string memory name, string memory lastname, string memory specialization) public {
-        uint256 offChainId = generateOffChainId(name, lastname, specialization);
-        require(medics[offChainId].isRegistered == true, "Medic not found");
-        Medic storage medic = medics[offChainId];
+    function updateMedic(string memory name, string memory lastname, string memory specialization) public onlyOwner {
+        require(medics[msg.sender].isRegistered, "Medic not found");
+        Medic storage medic = medics[msg.sender];
         medic.name = name;
         medic.lastname = lastname;
         medic.specialization = specialization;
         logAction("Update", msg.sender, "Medic updated");
+        emit EntityUpdated("Medic", msg.sender);
     }
 
     function addPatient(string memory name, string memory lastname, uint8 autonomous) public onlyOwner {
-        uint256 offChainId = generateOffChainId(name, lastname, "");
-        require(patients[offChainId].isRegistered == false, "Patient already registered");
-        Patient storage patient = patients[offChainId];
+         require(!patients[msg.sender].isRegistered, "Patient already registered");
+        Patient storage patient = patients[msg.sender];
         patient.name = name;
         patient.lastname = lastname;
         patient.autonomous = autonomous;
         patient.isRegistered = true;
-        patient.offChainId = offChainId;
         logAction("Create", msg.sender, "Patient added");
+        emit EntityRegistered("Patient", msg.sender);
     }
 
-    function updatePatient(string memory name, string memory lastname, uint8 autonomous) public {
-        uint256 offChainId = generateOffChainId(name, lastname, "");
-        require(patients[offChainId].isRegistered == true, "Patient not found");
-        Patient storage patient = patients[offChainId];
+    function updatePatient(string memory name, string memory lastname, uint8 autonomous) public onlyOwner {
+        require(patients[msg.sender].isRegistered, "Patient not found");
+        Patient storage patient = patients[msg.sender];
         patient.name = name;
         patient.lastname = lastname;
         patient.autonomous = autonomous;
         logAction("Update", msg.sender, "Patient updated");
+        emit EntityUpdated("Patient", msg.sender);
     }
 
     function addCaregiver(string memory name, string memory lastname) public onlyOwner {
-        uint256 offChainId = generateOffChainId(name, lastname, "");
-        require(caregivers[offChainId].isRegistered == false, "Caregiver already registered");
-        caregivers[offChainId] = Caregiver(name, lastname, true, offChainId);
+        require(caregivers[msg.sender].isRegistered == false, "Caregiver already registered");
+        caregivers[msg.sender] = Caregiver(name, lastname, true);
         logAction("Create", msg.sender, "Caregiver added");
+        emit EntityRegistered("Caregiver", msg.sender);
     }
 
-    function updateCaregiver(string memory name, string memory lastname) public {
-        uint256 offChainId = generateOffChainId(name, lastname, "");
-        require(caregivers[offChainId].isRegistered == true, "Caregiver not found");
-        Caregiver storage caregiver = caregivers[offChainId];
+    function updateCaregiver(string memory name, string memory lastname) public onlyOwner {
+        require(caregivers[msg.sender].isRegistered, "Caregiver not found");
+        Caregiver storage caregiver = caregivers[msg.sender];
         caregiver.name = name;
         caregiver.lastname = lastname;
         logAction("Update", msg.sender, "Caregiver status updated");
+        emit EntityUpdated("Caregiver", msg.sender);
     }
 
     function addReport(uint256 patientId, uint256 medicId, string memory details) public onlyOwner {
         uint256 reportId = uint256(keccak256(abi.encodePacked(patientId, medicId, details)));
         reports[reportId] = Report(reportId, patientId, medicId, details);
         logAction("Create", msg.sender, "Report added");
-    }
-
-    function updateReport(uint256 reportId, string memory newDetails) public {
-        require(keccak256(abi.encodePacked(reports[reportId].reportDetails)) != keccak256(abi.encodePacked(newDetails)), "No change in report details");
-        reports[reportId].reportDetails = newDetails;
-        logAction("Update", msg.sender, "Report updated");
     }
 
     function addTreatmentPlan(uint256 patientId, string memory details, string memory medication, uint256 startDate, uint256 endDate) public onlyOwner {
